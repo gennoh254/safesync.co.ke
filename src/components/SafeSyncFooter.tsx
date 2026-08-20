61ab78c8273aca73c8336649f11a968facbcd6f2
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2026-08-20 13:54:51 +0300

    udpated demo

--- src/components/SafeSyncFooter.tsx
+++ src/components/SafeSyncFooter.tsx
@@ -94,12 +94,12 @@ export default function SafeSyncFooter() {
               Legal
             </h3>
             <div className="flex flex-col gap-3 text-white/70">
-              <a
+              {/* <a
                 href="/privacy-policy"
                 className="hover:text-white transition"
               >
                 Privacy Policy
-              </a>
+              </a> */}
               <Link
                 to="/terms"
                 className="hover:text-white transition"
